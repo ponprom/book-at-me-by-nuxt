05862f74278b71b925965a6cfb96856b686efc4b
ponprom <ponprom.k2s@gmail.com>
2021-12-22 15:16:10 +0700

    fix books layout

--- components/layouts/BookLayout.vue
+++ components/layouts/BookLayout.vue
@@ -1,19 +1,32 @@
 <template>
   <div>
     <div class="min-h-screen">
+      <!-- <div>
+        <a-button :icon="'appstore'" ></a-button>
+      </div> -->
       <template v-for="(c, i) in bookList">
         <div
           v-if="i >= pageSize * (current - 1) && i < current * pageSize"
           :key="i"
-          class="inline-block h-56 p-2 sm:w-full md:w-1/4 lg:w-1/6 xl:w-1/6"
+          class="inline-block p-2 sm:w-full md:w-1/4 lg:w-1/6 xl:w-1/6"
+          style="height: 350px"
         >
-          <div class="bg-slate-500 h-full">
-            {{ i }}
+          <div
+            class="flex flex-col justify-items-center p-3 h-full shadow-md cursor-pointer hover:shadow-xl"
+          >
+            <div style="height: 250px; width: 172px truncate">
+              <img
+                :src="c.img"
+                class="w-full h-full"
+                style="border: 1px solid #ddd"
+              />
+            </div>
+            <div class="mt-2">{{ c.bookNameTh }}</div>
           </div>
         </div>
       </template>
     </div>
-    <div class="text-center">
+    <div class="text-center mt-10">
       <a-pagination
         v-model="current"
         :page-size="pageSize"
@@ -31,12 +44,15 @@ export default {
     bookList: {
       type: Array,
       default: null
+    },
+    pageSize: {
+      type: Number,
+      default: null
     }
   },
   data() {
     return {
-      current: 1,
-      pageSize: 18
+      current: 1
     }
   },
   methods: {
--- pages/novel/index.vue
+++ pages/novel/index.vue
@@ -1,9 +1,54 @@
 <template>
-  <div>novel</div>
+  <div>
+    novel
+    <book-layout :book-list="novels" :page-size="18" />
+  </div>
 </template>
 
 <script>
-export default {}
+import BookLayout from '@/components/layouts/BookLayout'
+export default {
+  components: {
+    BookLayout
+  },
+  data() {
+    return {
+      novels: [
+        {
+          isbn: '9786160439232',
+          bookNameTh: 'แฮร์รี่ พอตเตอร์กับห้องแห่งความลับ เล่ม 2',
+          bookNameEn: 'Harry Potter and the Chamber of Secrets',
+          price: 316,
+          authors: 'J.K. Rowling (เจ.เค. โรว์ลิ่ง)',
+          genre: [13,14,15,16],
+          type: 1,
+          desc: 'ช่วงหน้าร้อนของแฮรรี่ประกอบด้วยวันเกิดที่แย่ที่สุดที่เขาเคยเจอ คำเตือนที่เป็นลางร้ายจากด๊อบบี้ เอลฟ์ประจำบ้าน และการที่รอน วิสลีย์ เพื่อนรักของเขามาพาเขาหนีจากพวกเดอร์สลีย์ด้วยรถยนต์เหาะได้!เมื่อกลับไปเรียนปีสองที่โรงเรียนคาถาพ่อมดแม่มดและเวทมนตร์ศาสตร์ฮอกวอตส์ แฮรรี่ได้ยินเสียงกระซิบแปลกๆดังสะท้อนจากทางเดินว่างเปล่า แล้วการโจมตีก็เริ่มขึ้น นักเรียนหลายคนถุกพบว่ากลายเป็น หิน...ดูเหมือนว่าคำเตือนอันเป็นลางร้ายของด๊อบบี้กำลังจะกลายเป็นจริง',
+          img: ' https://drive.google.com/uc?export=view&id=1W9ObOBHqUZ91uAcYpl28IM9dbm2D_xVL'
+        }
+      ]
+    }
+  },
+  created() {
+    this.addNewNovel()
+  },
+  methods: {
+    addNewNovel() {
+      for (let index = 0; index < 15; index++) {
+        this.novels.push({
+          isbn: '9786164318861' + index,
+          bookNameTh: 'แฮร์รี่ พอตเตอร์กับห้องแห่งความลับ เล่ม 2',
+          bookNameEn: 'Harry Potter and the Chamber of Secrets',
+          price: 316,
+          authors: 'J.K. Rowling (เจ.เค. โรว์ลิ่ง)',
+          genre: [13,14,15,16],
+          type: 1,
+          desc: 'ช่วงหน้าร้อนของแฮรรี่ประกอบด้วยวันเกิดที่แย่ที่สุดที่เขาเคยเจอ คำเตือนที่เป็นลางร้ายจากด๊อบบี้ เอลฟ์ประจำบ้าน และการที่รอน วิสลีย์ เพื่อนรักของเขามาพาเขาหนีจากพวกเดอร์สลีย์ด้วยรถยนต์เหาะได้!เมื่อกลับไปเรียนปีสองที่โรงเรียนคาถาพ่อมดแม่มดและเวทมนตร์ศาสตร์ฮอกวอตส์ แฮรรี่ได้ยินเสียงกระซิบแปลกๆดังสะท้อนจากทางเดินว่างเปล่า แล้วการโจมตีก็เริ่มขึ้น นักเรียนหลายคนถุกพบว่ากลายเป็น หิน...ดูเหมือนว่าคำเตือนอันเป็นลางร้ายของด๊อบบี้กำลังจะกลายเป็นจริง',
+          img: ' https://drive.google.com/uc?export=view&id=1W9ObOBHqUZ91uAcYpl28IM9dbm2D_xVL'
+        })
+      }
+    }
+  }
+}
 </script>
 
 <style></style>
